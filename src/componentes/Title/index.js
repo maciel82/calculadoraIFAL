a26afbd37f555a7ae6ed6016cdf90ab74df28105
Maciel <maciel25.f@gmail.com>
2023-06-07 23:31:11 -0300

    adicionado absoluteFillObject

--- src/componentes/Title/index.js
+++ src/componentes/Title/index.js
@@ -3,6 +3,15 @@ import styles from './style'
 const Title = ({name})=>{
     return(
         <View style={styles.boxTitle}>
+            <View style={styles.box1}>
+                <Text style={styles.text}>1</Text>
+            </View>
+            <View style={styles.box2}>
+                <Text style={styles.text}>2</Text>
+            </View>
+            <View style={styles.box3}>
+                <Text style={styles.text}>3</Text>
+            </View>
             <Text style={styles.textTitle}>{name}</Text>
         </View>
     )
--- src/componentes/Title/style.js
+++ src/componentes/Title/style.js
@@ -10,14 +10,42 @@ const styles = StyleSheet.create({
         color:"#1E90FF",
         fontSize:34,
         fontWeight:"bold",
-        marginTop: 16,
+        marginTop: -30,
         paddingVertical: 8,
         paddingHorizontal: 8,
         borderWidth: 6,
         borderColor: '#1E90FF',
         borderRadius: 6,
         textAlign: 'center'
-    }
+    },
+    box1: {     
+        top: 0,
+        left: 1,
+        width: 50,
+        height: 50,
+        backgroundColor: 'red',
+      },
+      box2: {       
+        top: -20,
+        left: 30,
+        width: 40,
+        height: 40,
+        backgroundColor: 'blue',
+      },
+      box3: { 
+        top: -50,
+        left: -10,
+        width: 50,
+        height: 45,
+        backgroundColor: 'green',
+      },
+      text: {
+        color: '#FFF',
+        fontSize: 10,
+        fontWeight:"bold",
+        textAlign: "center",
+        marginTop: 16,
+      }
   });
 
   export default styles
